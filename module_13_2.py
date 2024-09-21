52093761f9848761a44a00e59f1ae9c7633364fb
Lexa622 <lexa622@gmail.com>
2024-09-21 17:21:32 +0300

    Домашнее задание по теме "Хендлеры обработки сообщений".

---
+++ module_13_2.py
@@ -0,0 +1,29 @@
+import asyncio
+from config import TOKEN
+from aiogram import Bot, Dispatcher, types
+from aiogram.filters.command import Command
+
+
+# Объект бота
+bot = Bot(token=TOKEN)
+# Диспетчер
+dp = Dispatcher()
+
+
+# Хэндлер на команду /start
+@dp.message(Command("start"))
+async def start(message: types.Message):
+    await message.answer('Привет! Я бот помогающий твоему здоровью.')
+    print('Привет! Я бот помогающий твоему здоровью.')
+
+
+# Хэндлер на все сообщения
+@dp.message()
+async def all_massages(message: types.Message):
+    print('Введите команду /start, чтобы начать общение.')
+    await message.answer('Введите команду /start, чтобы начать общение.')
+
+
+# Запуск процесса поллинга новых апдейтов
+if __name__ == "__main__":
+    asyncio.run(dp.start_polling(bot))
