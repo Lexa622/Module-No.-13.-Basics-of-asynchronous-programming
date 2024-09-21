88a0451c05f13b195cdd72edbc7185a4723f183c
Lexa622 <lexa622@gmail.com>
2024-09-21 19:54:45 +0300

    Домашнее задание по теме "Методы отправки сообщений".

--- module_13_3.py
+++ module_13_3.py
@@ -1,27 +1,26 @@
-import asyncio
 from config import TOKEN
-from aiogram import Bot, Dispatcher, types
-from aiogram.filters.command import Command
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
 
 
 # Объект бота
 bot = Bot(token=TOKEN)
 # Диспетчер
-dp = Dispatcher()
+dp = Dispatcher(bot, storage=MemoryStorage())
 
 
 # Хэндлер на команду /start
-@dp.message(Command("start"))
+@dp.message_handler(commands=['start'])
 async def start(message: types.Message):
     await message.answer('Привет! Я бот помогающий твоему здоровью.')
 
 
 # Хэндлер на все сообщения
-@dp.message()
+@dp.message_handler()
 async def all_massages(message: types.Message):
     await message.answer('Введите команду /start, чтобы начать общение.')
 
 
 # Запуск процесса поллинга новых апдейтов
 if __name__ == "__main__":
-    asyncio.run(dp.start_polling(bot))
+    executor.start_polling(dp, skip_updates=True)
